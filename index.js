634632e0e58e76a3bb808cc311c6879ebe8ebf23
Penelope Drago <prd@drago.com.au>
2023-03-16 13:13:37 +1100

    add functions

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function expression called divide
+const divide = function() {
+  return 2000/100;
+}
+
+// Arrow function called square
+const square = number => number * number;
+
+// Arrow function called add
+const add = (number1, number2) => {
+  return number1 + number2;
+}
